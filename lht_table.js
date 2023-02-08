ba9a7521842b4aee17211284d22f8378ea649dd3
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-02-08 10:11:33 -0600

    first commit

---
+++ lht_table.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: Jaedan
+   Date:  2/8/23
+
+	
+*/
+
+var thisDay = new Date('August 30, 2018');
+var tableHTML = "<table id='eventTable'><caption>Upcoming Events</caption><tr><th>Date</th><th>Event</th><th>Price</th></tr>";
+var endDate = new Date(this)
